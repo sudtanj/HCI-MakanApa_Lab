10e966722114f50a803e5f9e7149dfecfa8e584c
Sudono Tanjung <sudtanj@users.noreply.github.com>
2017-09-10 19:14:02 +0700

    Add files via upload

---
+++ sud_tanj.sql
@@ -0,0 +1,37 @@
+BEGIN TRANSACTION;
+INSERT INTO `recipes` (category_id,name,intro,instruction,image,link,time,servings,calories,favorite) VALUES (8,'Tango Berry Smoothie','Smoothies in the morning are an easy way to grab a nutritious cup of fruits and veggies especially for people who don''t like a big breakfast.
+
+This one is easy peasy. And it doesn''t have anything but fruit in it - no dairy. Throw in a handful of frozen mango, fresh or frozen strawberries, pineapple and a 2 peeled oranges. I add 1/2 c  water (more if needed). Blend away and it''s done! It honestly tastes like spring feels - clean, fresh, pure and refreshing. If you want you can add some hemp hearts for added nutrition.','Blend everything together for 1 minute or desired consistently is reached.','http://mock.robotemplates.com/cookbook/smoothie.jpg',NULL,60,4,240,0),
+ (8,'Sunset Cocktail',' Sunset Cocktail is easy to make into lovely color Layers.  The video shows you how!  Cool your party down, make it pretty and ramp up the fun!
+
+Happy me to have this delicious cocktail for a little kick back and cool down time. Hot, humid summer days call for cool relaxing refreshment. Lately, I have been having lots of fun experimenting with different blends of juices and liquor to make delicious party drinks.','Place 3-4 ice cubes in the bottom of a glass
+Pour the pineapple juice and the rum over the ice.
+Next, pour the grenadine slowly down one side of the glass (pouring too fast will mix the grenadine in and you won’t get the cool color separation)
+I stuck 2 cherries and a pineapple chunk on a toothpick for a garnish. Enjoy your drinks immediately.
+Please remember to drink responsibly and kick back and relax!','http://mock.robotemplates.com/cookbook/cocktail.jpg','',60,4,240,0),
+ (7,'Strawberry Cake','It took two whole months of flour-flying, cake-baking intensity, but we’ve come through for you with a strawberry cake that I think you’ll love. 100% homemade strawberry cake. Made with fresh strawberries.
+
+We had last month made a strawberry cake that was to-die-for that used a cake mix and gelatin, but you wanted a strawberry cake from scratch without gelatin. You asked for it, and we didn’t quit ’til we had it down. Ok….so I did quit, but then decided I couldn’t let the challenge go and picked it up again.
+
+Do you realize how hard it is to add fresh strawberries to homemade cake? It’s not as easy as dumping those juicy red berries into the batter. It takes some tweaking to get intense strawberry flavor without adding too much moisture to the cake. We finally made it happen and couldn’t be happier with the results!
+
+Let’s get right in to talking about this homemade strawberry cake recipe!','Preheat oven to 350 degrees F. Lightly grease 2 (9-inch) round cake pans.
+
+In a large bowl, combine cake mix and gelatin. Add pureed strawberries, eggs, oil, and water; beat at medium speed with an electric mixer until smooth. Pour into prepared pans, and bake for 20 minutes, or until a wooden pick inserted in the center comes out clean.
+
+Let cool in pans for 10 minutes. Remove from pans, and cool completely on wire racks.
+
+For the frosting:
+
+In a large bowl, beat butter and cream cheese at medium speed with an electric mixer until creamy. Beat in 1/4 cup of the strawberry puree and the strawberry extract. (The rest of the puree is leftover but can be used in smoothies or on ice cream for a delicious treat.) Gradually add confectioners'' sugar, beating until smooth.
+
+Spread frosting in between layers and on top and sides of cake. Garnish with sliced fresh strawberries, if desired.','http://mock.robotemplates.com/cookbook/cake.jpg','',60,4,240,0),
+ (7,'Meringue Cake With Red Currant Curd Filling','„Coffee break! Here is a summery and fruity cake that will lift your mood and energy levels. For my red currant cake, I used less sugar. It tastes refreshingly tangy, but also sweet with red currants and fluffy meringue. ','Preheat the oven to 180°C/350°F. To make the cake base, whisk together sugar, vanilla sugar, egg yolks, and soft butter. Add flour, baking powder, and milk, and mix to combine. Fill the batter into a greased and flour-dusted springform pan. Bake at 180°C/350°F for approx. 15 min. Set aside.
+
+To make the meringue, beat egg whites until foamy. Then add sugar and vanilla sugar and continue to beat until stiff peaks form. Gently fold the freshly plucked, washed, and destemmed red currants through half of the meringue. Now spread this mixture on top of the cake base, topped by the remaining meringue. Bake again at 180°C/350°F for approx. 15 min.
+
+Set cake aside to cool, then transfer to the fridge.','http://mock.robotemplates.com/cookbook/meringue.jpg','',60,4,240,0),
+ (7,'Lemon Berry Trifle','"I needed to make a dessert last minute for a party and went through my cupboards and found instant lemon pudding mix so I came up with this recipe. It was so tasty and delicious."','To make the lemon curd: Bring a pot of water to a simmer over medium-low heat. Combine the egg yolks, sugar, lemon juice, and zest in a metal or glass heat-resistant bowl and whisk until smooth. Set the bowl over the simmering water, without letting the bottom touch, and continue to whisk. Keep working-out that arm and whisk it vigorously for a good 10 minutes, until the curd has doubled in volume and is very thick and yellow. Don''t let it boil. Remove the bowl from heat and whisk in the butter, a couple of chunks at a time, until melted. Refrigerate until the custard is cold and firm.
+
+To build the trifle: put the berries in a mixing bowl and toss them together so they are evenly distributed. Fold the whipped cream into the chilled lemon curd to lighten it up into a mousse. Line a glass trifle bowl with pieces of pound cake to fit. Drizzle or brush the cake with the Limoncello, spoon a layer of the lemon curd over the cake, and then a layer of mixed berries. Repeat the layers until the ingredients are used up, the last layer looks best if it''s the berries. Chill before serving. Garnish with fresh mint.','http://mock.robotemplates.com/cookbook/trifle.jpg','',60,4,240,0);
+COMMIT;
