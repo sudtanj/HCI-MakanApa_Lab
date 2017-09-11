9dae1632b2ce1c00ccaebe742fc737f75ada2d3d
Sudono Tanjung <sudtanj@users.noreply.github.com>
2017-09-12 06:32:08 +0700

    Add files via upload

---
+++ feronia.sql
@@ -0,0 +1,61 @@
+BEGIN TRANSACTION;
+INSERT INTO `recipes` VALUES (7,'Key Lime Pie','Key limes are also known as Mexican or West Indian limes. If you can''t find them in your area, substitute bottled Key lime juice. We''ve tried several different brands in our test kitchens, and prefer the taste of Manhattan.* This recipe is modified from the classic one found on many condensed milk and Key lime juice labels; we''ve added additional lime juice for more tartness.','Make crust:
+
+    Preheat oven to 350°F.
+    Stir together graham cracker crumbs, sugar, and butter in a bowl with a fork until combined well, then press mixture evenly onto bottom and up side of a 9-inch (4-cup) glass pie plate.
+    Bake crust in middle of oven 10 minutes and cool in pie plate on a rack. Leave oven on. 
+
+Make filling and bake pie:
+
+    Whisk together condensed milk and yolks in a bowl until combined well. Add juice and whisk until combined well (mixture will thicken slightly).
+    Pour filling into crust and bake in middle of oven 15 minutes. Cool pie completely on rack (filling will set as it cools), then chill, covered, at least 8 hours. 
+
+Make topping:
+
+    Just before serving, beat cream in a bowl with an electric mixer until it just holds stiff peaks. Serve pie topped with cream. ','http://mock.robotemplates.com/cookbook/key.jpg','',70,5,300,0);
+INSERT INTO `recipes` VALUES (7,'Cupcake','Traditional cupcakes are always a crowd-pleaser. Exercise your creative muscle by decorating these cupcakes to suit your mood. That is this cupcake.',' Step 1
+    Preheat oven to 200C or 180C fan-forced. Grease a 12 x 1/3-cup capacity muffin pan. Alternatively, line holes with paper cases. Combine flour and caster sugar in a bowl. Make a well in the centre.
+
+ Step 2
+    Add milk, butter, eggs and vanilla to flour mixture. Using a large metal spoon, stir gently to combine.
+
+ Step 3
+    Spoon mixture into prepared muffin pan. Bake for 12 to 15 minutes, or until a skewer inserted into the centre comes out clean. Stand in pan for 5 minutes before transferring to a wire rack to cool.
+
+  Step 4
+    Make icing: Sift icing sugar into a bowl. Add food colouring and water. Stir until smooth and well combined. Spoon icing over cupcakes. Decorate with sprinkles.
+','http://mock.robotemplates.com/cookbook/cupcake.jpg','',60,6,240,0);
+INSERT INTO `recipes` VALUES (6,'Summer Salad With Tomatoes and Basil','This salad is an absolute winner.  It''s so easy to make.  It''s so light and refreshing.  It tastes like summer!  Like others have said, it can be modified- I like to put sliced red onion in it- but it tastes great as is too.  I serve it alone, on top of flatbread or on top of baguette rounds.  And it''s a perfect addition to BBQs to give an alternative to the heavy, mayo-based salads. Love it!','Step 1
+    In a small skillet, heat the olive oil over medium-high heat. Add the garlic and stir, lightly frying for about a minute and removing from the heat before the garlic gets too brown (it can be golden). 
+
+Step 2
+    Pour it into a mixing bowl and allow to cool slightly. Add the tomatoes, balsamic, basil and some salt and pepper to the bowl. 
+
+Step 3
+    Toss to combine, and then taste and add more basil if needed, and more salt if needed.','http://mock.robotemplates.com/cookbook/salad.jpg','',30,3,200,0);
+INSERT INTO `recipes` VALUES (4,'Tuna Steak','Firm-textured tuna steaks are a great fish for grilling. Pair them with an easy sauce of fresh tomato and mayonnaise.','Step 1
+    Thaw fish, if frozen.
+
+Step 2
+    For sauce, in a small bowl combine tomato, onion, basil, mayonnaise or salad dressing, garlic, and salt. Cover and chill until serving time.
+
+Step 3
+    Brush both sides of fish with oil. Grill fish steaks on the greased rack of an uncovered grill directly over medium coals for 8 to 12 minutes or just until fish begins to flake easily when tested with a fork, turning once and brushing occasionally with oil. Serve with sauce. Makes 4 servings.','http://mock.robotemplates.com/cookbook/tuna.jpg','',55,4,350,0);
+INSERT INTO `recipes` VALUES (4,'Posole and Quesadillas','Spanish for hominy, Posole is hulled corn kernel that have been soaked in an alkaline lime solution to remove the husks, then dried. The constant ingredients are stock made by cooking pork, beef, or chicken with onion, farlic, chilles, and Posole.','Step 1
+    Heat oil over low heat. Add garlic, onion, and tomatillas. Cook until softened. Add stock. Coarsely chop 3/4 of the peppers and add to the stock. Cook on low simmer for about 15 minutes until flavors blend.
+
+Step 2
+    Chop leaves from one bunch of cilantro, and add to soup along with cooked turkey and hominy.
+
+Step 3
+    While the soup is cooking, prepare condiments like shave radishes, julienne cut slices into strips, then sprinkle them and cabbage with fresh lime juice. Mince remaining chili peppers. Put radishes, minced peppers, toasted, Pepitas, cabbage and chopped tomatoes into separate condiment dishes.
+
+Step 4
+    Taste soup and correct seasoning with salt and dried Chipotle. Bear in mind that the heat from the Tabasco and chili peppers will increase as the soup cooks.
+
+Step 5
+    Just before serving add remaining chopped cilantro.
+
+Step 6
+    Ladlle soup into heated bowls and serve with condiments and Quesadillas.','http://mock.robotemplates.com/cookbook/quesadillas.jpg','',45,6,360,0);
+COMMIT;
