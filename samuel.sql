e11d54fff3bb4d091a8eae59e80812812e29f5a2
Sudono Tanjung <sudtanj@users.noreply.github.com>
2017-09-13 20:07:29 +0700

    Add files via upload

---
+++ samuel.sql
@@ -0,0 +1,132 @@
+BEGIN TRANSACTION;
+INSERT INTO `recipes` (category_id,name,intro,instruction,image,link,time,servings,calories,favorite) VALUES (7,'Key Lime Pie','Key limes are also known as Mexican or West Indian limes. If you can''t find them in your area, substitute bottled Key lime juice. We''ve tried several different brands in our test kitchens, and prefer the taste of Manhattan.* This recipe is modified from the classic one found on many condensed milk and Key lime juice labels; we''ve added additional lime juice for more tartness.','Make crust:
+
+    Preheat oven to 350°F.
+    Stir together graham cracker crumbs, sugar, and butter in a bowl with a fork until combined well, then press mixture evenly onto bottom and up side of a 9-inch (4-cup) glass pie plate.
+    Bake crust in middle of oven 10 minutes and cool in pie plate on a rack. Leave oven on. 
+
+Make filling and bake pie:
+
+    Whisk together condensed milk and yolks in a bowl until combined well. Add juice and whisk until combined well (mixture will thicken slightly).
+    Pour filling into crust and bake in middle of oven 15 minutes. Cool pie completely on rack (filling will set as it cools), then chill, covered, at least 8 hours. 
+
+Make topping:
+
+    Just before serving, beat cream in a bowl with an electric mixer until it just holds stiff peaks. Serve pie topped with cream. ','http://mock.robotemplates.com/cookbook/key.jpg','',70,5,300,0),
+ (7,'Cupcake','Traditional cupcakes are always a crowd-pleaser. Exercise your creative muscle by decorating these cupcakes to suit your mood. That is this cupcake.',' Step 1
+    Preheat oven to 200C or 180C fan-forced. Grease a 12 x 1/3-cup capacity muffin pan. Alternatively, line holes with paper cases. Combine flour and caster sugar in a bowl. Make a well in the centre.
+
+ Step 2
+    Add milk, butter, eggs and vanilla to flour mixture. Using a large metal spoon, stir gently to combine.
+
+ Step 3
+    Spoon mixture into prepared muffin pan. Bake for 12 to 15 minutes, or until a skewer inserted into the centre comes out clean. Stand in pan for 5 minutes before transferring to a wire rack to cool.
+
+  Step 4
+    Make icing: Sift icing sugar into a bowl. Add food colouring and water. Stir until smooth and well combined. Spoon icing over cupcakes. Decorate with sprinkles.
+','http://mock.robotemplates.com/cookbook/cupcake.jpg','',60,6,240,0),
+ (6,'Summer Salad With Tomatoes and Basil','This salad is an absolute winner.  It''s so easy to make.  It''s so light and refreshing.  It tastes like summer!  Like others have said, it can be modified- I like to put sliced red onion in it- but it tastes great as is too.  I serve it alone, on top of flatbread or on top of baguette rounds.  And it''s a perfect addition to BBQs to give an alternative to the heavy, mayo-based salads. Love it!','Step 1
+    In a small skillet, heat the olive oil over medium-high heat. Add the garlic and stir, lightly frying for about a minute and removing from the heat before the garlic gets too brown (it can be golden). 
+
+Step 2
+    Pour it into a mixing bowl and allow to cool slightly. Add the tomatoes, balsamic, basil and some salt and pepper to the bowl. 
+
+Step 3
+    Toss to combine, and then taste and add more basil if needed, and more salt if needed.','http://mock.robotemplates.com/cookbook/salad.jpg','',30,3,200,0),
+ (4,'Tuna Steak','Firm-textured tuna steaks are a great fish for grilling. Pair them with an easy sauce of fresh tomato and mayonnaise.','Step 1
+    Thaw fish, if frozen.
+
+Step 2
+    For sauce, in a small bowl combine tomato, onion, basil, mayonnaise or salad dressing, garlic, and salt. Cover and chill until serving time.
+
+Step 3
+    Brush both sides of fish with oil. Grill fish steaks on the greased rack of an uncovered grill directly over medium coals for 8 to 12 minutes or just until fish begins to flake easily when tested with a fork, turning once and brushing occasionally with oil. Serve with sauce. Makes 4 servings.','http://mock.robotemplates.com/cookbook/tuna.jpg','',55,4,350,0),
+ (4,'Posole and Quesadillas','Spanish for hominy, Posole is hulled corn kernel that have been soaked in an alkaline lime solution to remove the husks, then dried. The constant ingredients are stock made by cooking pork, beef, or chicken with onion, farlic, chilles, and Posole.','Step 1
+    Heat oil over low heat. Add garlic, onion, and tomatillas. Cook until softened. Add stock. Coarsely chop 3/4 of the peppers and add to the stock. Cook on low simmer for about 15 minutes until flavors blend.
+
+Step 2
+    Chop leaves from one bunch of cilantro, and add to soup along with cooked turkey and hominy.
+
+Step 3
+    While the soup is cooking, prepare condiments like shave radishes, julienne cut slices into strips, then sprinkle them and cabbage with fresh lime juice. Mince remaining chili peppers. Put radishes, minced peppers, toasted, Pepitas, cabbage and chopped tomatoes into separate condiment dishes.
+
+Step 4
+    Taste soup and correct seasoning with salt and dried Chipotle. Bear in mind that the heat from the Tabasco and chili peppers will increase as the soup cooks.
+
+Step 5
+    Just before serving add remaining chopped cilantro.
+
+Step 6
+    Ladlle soup into heated bowls and serve with condiments and Quesadillas.','http://mock.robotemplates.com/cookbook/quesadillas.jpg','',45,6,360,0),
+ (3,'Vegetable Soup','Vegetable soup is a common soup prepared using vegetables and leaf vegetables as primary ingredients. It dates to ancient history, and is a mass-produced food product in contemporary times.','Step 1   
+    Heat oil in a large soup or stock pot. Add the chopped onion, minced garlic, the carrots, and the celery. Heat, stirring, for 3-4 minutes, until onions are soft. Add the rest of the vegetables and heat for just another minute or two.
+
+Step 2
+    Add salt, garlic powder, thyme, and oregano, stirring to coat the vegetables well.
+
+Step 3
+    Add bay leaf, water or vegetable broth and bring to a simmer. If using bouillon cubes, add and stir to make sure they dissolve.
+
+Step 4
+    Add the tomatoes, stir, and allow your vegetable soup to simmer until all the vegetables are cooked, about 8-12 minutes.
+
+Step 5
+    Adjust seasonings to taste.
+
+Step6
+    Remove the bay leaf and serve your homemade vegetable soup with a sprinkle of Parmesan cheese if you''re not eating vegan, and some crusty bread or crackers.','http://mock.robotemplates.com/cookbook/vegetablesoup.jpg','',60,4,240,0),
+ (3,'Potato Soup','Warm, comforting, and so delicious, this classic potato soup is the ultimate bowl of comfort food. Becuase more cheese is always better, this classic soup gets a double hit of cheese--both stirred into the soup and sprinkled over the top. Bacon bits lend a baked potato flavor and feel to this creamy, crowd-pleasing soup. Simply place the ingredients into a slow cooker and let it do all of the work for you.','Step 1
+   Cook bacon in a large nonstick skillet over medium heat until crisp. Remove bacon from pan, reserving 2 teaspoons drippings in pan; crumble bacon. Add onion to drippings in pan; sauté 3 minutes or until tender.
+
+Step 2
+   Place potato slices and onion in a 5-quart electric slow cooker coated with cooking spray. Combine 1/2 cup water and next 3 ingredients (through pepper); stir into mixture in slow cooker. Cover and cook on LOW for 8 hours or until potatoes are tender.
+
+Step 3
+   Mash mixture with a potato masher; stir in milk and 3/4 cup cheese. Increase heat to HIGH. Cover and cook on HIGH for 20 minutes or until mixture is thoroughly heated. Ladle soup into bowls. Top with sour cream and remaining 1/4 cup cheese. Sprinkle with bacon and chives.
+','http://mock.robotemplates.com/cookbook/potatosoup.jpg','',60,4,240,0),
+ (2,'Sushi Rolls','Sushi can be filled with any ingredients you choose. Try smoked salmon instead of imitation crabmeat. Serve with teriyaki sauce and wasabi.','Step 1
+    In a medium saucepan, bring 1 1/3 cups water to a boil. Add rice, and stir. Reduce heat, cover, and simmer for 20 minutes. In a small bowl, mix the rice vinegar, sugar ,and salt. Blend the mixture into the rice.
+
+Step 2
+    Preheat oven to 300 degrees F (150 degrees C). On a medium baking sheet, heat nori in the preheated oven 1 to 2 minutes, until warm.
+
+Step 3
+    Center one sheet nori on a bamboo sushi mat. Wet your hands. Using your hands, spread a thin layer of rice on the sheet of nori, and press into a thin layer. Arrange 1/4 of the cucumber, ginger, avocado, and imitation crabmeat in a line down the center of the rice. Lift the end of the mat, and gently roll it over the ingredients, pressing gently. Roll it forward to make a complete roll. Repeat with remaining ingredients.
+
+Step 4
+    Cut each roll into 4 to 6 slices using a wet, sharp knife.
+','http://mock.robotemplates.com/cookbook/sushi.jpg','',60,4,240,0),
+ (2,'Tuna Nachos','These are absolutely incredibly insanely delicious.  Fried wontons are topped with seaweed salad, seared tuna, tomatoes, avocado and sesame seeds… then drizzled with wasabi mayo and sriracha.','Step 1
+    Prepare the marinade for the tuna by whisking together soy sauce, sesame oil, garlic powder, salt and pepper. Place tuna steaks in a Ziploc bag and pour in marinade. Coat well and place in your refrigerator.
+
+Step 2
+    While tuna is marinating, prepare wasabi mayo by whisking ingredients together in a small bowl. Cover and refrigerate.
+
+Step 3
+    Prepare fried wontons by heating vegetable oil in a large heavy pot or skillet with high sides over medium high heat (2-inches deep) until it reaches 350 degrees F, using a deep-fry thermometer.
+
+Step 4
+    Add wontons to oil and fry, working in batches, until chips are slightly puffy and golden brown, stirring gently (about 1 minute). Adjust heat as necessary to maintain a temperature of 350 degrees F. Transfer to a large plate or baking sheet lined with paper towels.
+
+Sear the tuna:
+
+Step 1
+    Heat a nonstick skillet over medium high heat until hot. Remove tuna steaks from marinade and sear for 1 minute on each side, or until desired temperature is met (my tuna steaks were 1-1/2 inches thick and seared 1 minute on each side to yield rare).
+
+Step 2
+    Remove tuna steaks from pan and thinly slice into approximately 1-inch pieces.
+
+Prepare the nachos:
+
+Step 1
+    Assemble nachos by evenly distributing fried wontons onto 2 plates. Top each with ½ of the seaweed salad, followed by the sliced tuna, avocado, tomatoes and sesame seeds.
+    
+Step 2
+    Drizzle with desired amount of wasabi mayo and sriracha. Serve with additional soy sauce, if desired.
+','http://mock.robotemplates.com/cookbook/nachos.jpg','',60,4,240,0),
+ (1,'Belgian Waffle','Belgian waffles are a variety of waffle with a lighter batter, larger squares, and deeper pockets than ordinary American waffles. toppings vary from whipped cream, confectioners sugar, soft fruit, and chocolate spread, to syrup and butter or margarine. They may also be served with vanilla ice cream and fresh fruit (such as strawberries) as a dessert.','Step 1
+   In a bowl, combine flour, sugar and baking powder. In another bowl, lightly beat egg yolks. Add milk, butter and vanilla; mix well. Stir into dry ingredients just until combined. Beat egg whites until stiff peaks form; fold into batter.
+
+Step 2
+   Bake in a preheated waffle iron according to manufacturer''s directions until golden brown. Serve with strawberries or syrup. Yield: 10 waffles (about 4-1/2 inches). ','http://mock.robotemplates.com/cookbook/waffle.jpg','',60,4,240,0);
+COMMIT;
