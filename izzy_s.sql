a14a1e289d15cf8a20f702718520b495e87f5fc1
Sudono Tanjung <sudtanj@users.noreply.github.com>
2017-09-14 18:27:00 +0700

    Add files via upload

---
+++ izzy_s.sql
@@ -0,0 +1,102 @@
+BEGIN TRANSACTION;
+INSERT INTO `recipes` (category_id,name,intro,instruction,image,link,time,servings,calories,favorite) VALUES (8,'Tango Berry Smoothie','Smoothies in the morning are an easy way to grab a nutritious cup of fruits and veggies especially for people who don''t like a big breakfast.
+
+This one is easy peasy. And it doesn''t have anything but fruit in it - no dairy. Throw in a handful of frozen mango, fresh or frozen strawberries, pineapple and a 2 peeled oranges. I add 1/2 c  water (more if needed). Blend away and it''s done! It honestly tastes like spring feels - clean, fresh, pure and refreshing. If you want you can add some hemp hearts for added nutrition.','Blend everything together for 1 minute or desired consistently is reached.','http://mock.robotemplates.com/cookbook/smoothie.jpg',NULL,60,4,240,0),
+ (8,'Sunset Cocktail',' Sunset Cocktail is easy to make into lovely color Layers.  The video shows you how!  Cool your party down, make it pretty and ramp up the fun!
+
+Happy me to have this delicious cocktail for a little kick back and cool down time. Hot, humid summer days call for cool relaxing refreshment. Lately, I have been having lots of fun experimenting with different blends of juices and liquor to make delicious party drinks.','Place 3-4 ice cubes in the bottom of a glass
+Pour the pineapple juice and the rum over the ice.
+Next, pour the grenadine slowly down one side of the glass (pouring too fast will mix the grenadine in and you won’t get the cool color separation)
+I stuck 2 cherries and a pineapple chunk on a toothpick for a garnish. Enjoy your drinks immediately.
+Please remember to drink responsibly and kick back and relax!','http://mock.robotemplates.com/cookbook/cocktail.jpg','',60,4,240,0),
+ (7,'Strawberry Cake','It took two whole months of flour-flying, cake-baking intensity, but we’ve come through for you with a strawberry cake that I think you’ll love. 100% homemade strawberry cake. Made with fresh strawberries.
+
+We had last month made a strawberry cake that was to-die-for that used a cake mix and gelatin, but you wanted a strawberry cake from scratch without gelatin. You asked for it, and we didn’t quit ’til we had it down. Ok….so I did quit, but then decided I couldn’t let the challenge go and picked it up again.
+
+Do you realize how hard it is to add fresh strawberries to homemade cake? It’s not as easy as dumping those juicy red berries into the batter. It takes some tweaking to get intense strawberry flavor without adding too much moisture to the cake. We finally made it happen and couldn’t be happier with the results!
+
+Let’s get right in to talking about this homemade strawberry cake recipe!','Preheat oven to 350 degrees F. Lightly grease 2 (9-inch) round cake pans.
+
+In a large bowl, combine cake mix and gelatin. Add pureed strawberries, eggs, oil, and water; beat at medium speed with an electric mixer until smooth. Pour into prepared pans, and bake for 20 minutes, or until a wooden pick inserted in the center comes out clean.
+
+Let cool in pans for 10 minutes. Remove from pans, and cool completely on wire racks.
+
+For the frosting:
+
+In a large bowl, beat butter and cream cheese at medium speed with an electric mixer until creamy. Beat in 1/4 cup of the strawberry puree and the strawberry extract. (The rest of the puree is leftover but can be used in smoothies or on ice cream for a delicious treat.) Gradually add confectioners'' sugar, beating until smooth.
+
+Spread frosting in between layers and on top and sides of cake. Garnish with sliced fresh strawberries, if desired.','http://mock.robotemplates.com/cookbook/cake.jpg','',60,4,240,0),
+ (7,'Meringue Cake With Red Currant Curd Filling','„Coffee break! Here is a summery and fruity cake that will lift your mood and energy levels. For my red currant cake, I used less sugar. It tastes refreshingly tangy, but also sweet with red currants and fluffy meringue. ','Preheat the oven to 180°C/350°F. To make the cake base, whisk together sugar, vanilla sugar, egg yolks, and soft butter. Add flour, baking powder, and milk, and mix to combine. Fill the batter into a greased and flour-dusted springform pan. Bake at 180°C/350°F for approx. 15 min. Set aside.
+
+To make the meringue, beat egg whites until foamy. Then add sugar and vanilla sugar and continue to beat until stiff peaks form. Gently fold the freshly plucked, washed, and destemmed red currants through half of the meringue. Now spread this mixture on top of the cake base, topped by the remaining meringue. Bake again at 180°C/350°F for approx. 15 min.
+
+Set cake aside to cool, then transfer to the fridge.','http://mock.robotemplates.com/cookbook/meringue.jpg','',60,4,240,0),
+ (7,'Lemon Berry Trifle','"I needed to make a dessert last minute for a party and went through my cupboards and found instant lemon pudding mix so I came up with this recipe. It was so tasty and delicious."','To make the lemon curd: Bring a pot of water to a simmer over medium-low heat. Combine the egg yolks, sugar, lemon juice, and zest in a metal or glass heat-resistant bowl and whisk until smooth. Set the bowl over the simmering water, without letting the bottom touch, and continue to whisk. Keep working-out that arm and whisk it vigorously for a good 10 minutes, until the curd has doubled in volume and is very thick and yellow. Don''t let it boil. Remove the bowl from heat and whisk in the butter, a couple of chunks at a time, until melted. Refrigerate until the custard is cold and firm.
+
+To build the trifle: put the berries in a mixing bowl and toss them together so they are evenly distributed. Fold the whipped cream into the chilled lemon curd to lighten it up into a mousse. Line a glass trifle bowl with pieces of pound cake to fit. Drizzle or brush the cake with the Limoncello, spoon a layer of the lemon curd over the cake, and then a layer of mixed berries. Repeat the layers until the ingredients are used up, the last layer looks best if it''s the berries. Chill before serving. Garnish with fresh mint.','http://mock.robotemplates.com/cookbook/trifle.jpg','',60,4,240,0),
+ (2,'Rings Calamari','Home made calamari rings. This recipe is super easy to make and simply delicious!','1. Stir flour, 2 teaspoons salt, paprika, and black pepper together in a shallow bowl.
+2. Heat oil in a deep-fryer or large saucepan to 375 degrees F (190 degrees C).
+3. Press calamari rings into the flour mixture until evenly coated, shaking off excess flour.
+4. Working in batches, fry calamari rings in the hot oil until golden brown, 3 to 4 minutes. Transfer cooked calamari to a paper towel-lined plate; sprinkle with salt. Serve calamari with cocktail and lemon wedges.','http://mock.robotemplates.com/cookbook/calamari.jpg',NULL,20,4,532,0),
+ (2,'Hot Gingered Prawns','I’ve never been to a party where anyone was ever disappointed when they were served shrimp as an appetizer. Put them on a stick, spice them up, and serve them four at a time, and your guests are sure to be even happier. Super-size them by using large prawns and you have a hit.','Thread prawns on skewers.','http://mock.robotemplates.com/cookbook/prawns.jpg',NULL,60,4,240,0),
+ (1,'English Breakfast','“Some things are too good to mess about with, so I''ve kept my English breakfast pretty old-school ”','Preheat the grill to high. Set the oven to its lowest heat and pop 2 plates in it to keep warm. Put the sausages, mushrooms and tomatoes, scored-side up, on to a large grill pan and place under the grill, about 5cm from the heat. Cook for about 10 minutes, turning the sausages once or twice.','http://mock.robotemplates.com/cookbook/english.jpg',NULL,30,2,1172,0),
+ (1,'Baked Biscuits','This is a basic biscuit recipe with baking powder used as the leavening. They''re easy to make and go with almost any meal.','Add the bacon and black pudding to the pan and grill for 5 minutes, turning halfway through cooking, until they are cooked and crispy. Put the baked beans in a saucepan and warm gently for 2 to 3 minutes, stirring occasionally.','http://mock.robotemplates.com/cookbook/biscuits.jpg',NULL,25,10,191,0),
+ (2,'Eggs With Crab Dip','This classic New Orleans dish is perfect for a Mardi Gras party appetizer!','1. In a large mixing bowl, add the mayonnaise, mustard, lemon juice and zest, hot sauce and salt and pepper to taste. Whisk until smooth. Add the shallots, chives, capers, tarragon, and egg. Mix to combine. Add the crab and gently fold to combine.
+2. For the Toast Points: preheat oven to 375ºF. Remove the crusts from the bread and cut into 4 triangles. Place on baking sheet and toast until golden brown, about 7-9 minutes. Allow to cool before serving.
+3. Serve crab dip with endive leaves and toast points. Garnish with more chives and chopped egg, if desired.
+4. Tip: make the dip up to one day ahead of time! Cover and store in the refrigerator until ready to serve.','http://mock.robotemplates.com/cookbook/eggs.jpg',NULL,60,4,240,0);
+INSERT INTO `ingredients` (recipe_id,name,quantity,unit) VALUES (6,'all-purpose flour',1.0,'cup'),
+ (6,'salt',2.0,'teaspoon'),
+ (6,'paprika',1.0,'teaspoon'),
+ (6,'ground black pepper','1/4','teaspoon'),
+ (6,'garlic powder',1.0,'teaspoon'),
+ (6,'vegetable oil for frying',6.0,'cups'),
+ (6,'frozen calamari rings, thawed and drained',12.0,'ounces'),
+ (6,'lemon',2.0,'tablespoon'),
+ (6,'cocktail sauce',1.0,NULL),
+ (8,'higher-welfare pork and leek sausages',4.0,NULL),
+ (8,'field mushrooms , wiped',4.0,NULL),
+ (8,'small tomatoes , scored on top',2.0,NULL),
+ (8,'rashers higher-welfare smoked dry cured back bacon',4.0,NULL),
+ (8,'black pudding , sliced',100.0,'g'),
+ (8,'baked beans',420.0,'g'),
+ (8,' butter',2.0,'knobs'),
+ (8,'free-range eggs , cracked into a bowl and beaten',4.0,NULL),
+ (8,'sea salt',NULL,NULL),
+ (8,'freshly ground black pepper',NULL,NULL),
+ (8,'thick slices bread , for toasting','2-4',NULL),
+ (9,'all-purpose flour',2.0,'cups'),
+ (9,' baking powder',1.0,'tablespoon'),
+ (9,'salt','1/2','teaspoon'),
+ (9,'shortening','1/2','cup'),
+ (9,'milk','3/4','cup'),
+ (7,'green king prawns, peeled with tails intact','2 1/4','pounds'),
+ (7,'crushed garlic',1.0,'teaspoon'),
+ (7,'soy sauce','1/4','cup'),
+ (7,'dry white wine','1/4','cup'),
+ (7,'finely shredded ginger',1.0,'tablespoon'),
+ (7,'Thai sweet chili sauce',12.0,'ounces'),
+ (7,'wooden skewers soaked in water','',''),
+ (10,'mayonnaise','1/2','cup'),
+ (10,'Dijon mustard',2.0,'teaspoon'),
+ (10,'lemon (juice and zest)','1/2',''),
+ (10,'Louisiana-style hot sauce','1-2','teaspoon'),
+ (10,'shallots (finely chopped)',2.0,'teaspoon'),
+ (10,'chives (finely chopped)',2.0,'teaspoon'),
+ (10,'capers (drained)',1.0,'tablespoon'),
+ (10,'tarragon (finely chopped)',1.0,'tablespoon'),
+ (10,'egg (finely chopped)',1.0,NULL),
+ (10,'jumbo lump crab meat (picked through for shells)',1.0,'pound'),
+ (10,'heads endive (root end removed, leaves separated, to serve)',2.0,NULL),
+ (10,'white bread for toast points (to serve)','1/2','loaf'),
+ (10,'Kosher salt and freshly ground black pepper (to taste)',NULL,NULL);
+INSERT INTO `categories` (id,name,image) VALUES (1,'Breakfast','assets://categories/breakfast.png'),
+ (2,'Appetizers','assets://categories/appetizers.png'),
+ (3,'Soups','assets://categories/soups.png'),
+ (4,'Main courses','assets://categories/main.png'),
+ (5,'Side dishes','assets://categories/side.png'),
+ (6,'Vegetarian','assets://categories/vegetarian.png'),
+ (7,'Desserts','assets://categories/desserts.png'),
+ (8,'Beverages','assets://categories/beverages.png'),
+ (9,'Other','assets://categories/other.png');
+COMMIT;
